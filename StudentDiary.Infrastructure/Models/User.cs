545c01cb69fec0c20adeac96528b60cc52bed959
Bianca Dollente <biancaadrienne24@gmail.com>
2025-10-03 22:12:09 +0800

    fix: - bug of API Tiny TinyMCE

--- StudentDiary.Infrastructure/Models/User.cs
+++ StudentDiary.Infrastructure/Models/User.cs
@@ -5,36 +5,36 @@ namespace StudentDiary.Infrastructure.Models
     public class User
     {
         public int Id { get; set; }
-        
+
         [Required]
         [StringLength(50)]
         public string Username { get; set; }
-        
+
         [Required]
         [StringLength(100)]
         public string Email { get; set; }
-        
+
         [Required]
         public string PasswordHash { get; set; }
-        
+
         [StringLength(50)]
-        public string FirstName { get; set; }
-        
+        public string? FirstName { get; set; }
+
         [StringLength(50)]
-        public string LastName { get; set; }
-        
-        public string ProfilePicturePath { get; set; }
-        
+        public string? LastName { get; set; }
+
+        public string? ProfilePicturePath { get; set; }
+
         public DateTime DateCreated { get; set; }
-        
+
         public DateTime LastLoginDate { get; set; }
-        
+
         public int FailedLoginAttempts { get; set; }
-        
+
         public DateTime? LockoutEnd { get; set; }
-        
-        public string PasswordResetToken { get; set; }
-        
+
+        public string? PasswordResetToken { get; set; }
+
         public DateTime? PasswordResetTokenExpiry { get; set; }
 
         //Navigation Property
